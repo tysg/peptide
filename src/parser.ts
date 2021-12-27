e6a030f8f8765276901f2a3f1bad018fb50aa6da
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-12-27 16:20:28 +0800

    Setup CI, unit tests and formatter (#1)
    * Format with prettier
    * CI only runs on MacOS

--- src/parser.ts
+++ src/parser.ts
@@ -1,31 +1,46 @@
-import * as path from 'path'
-import * as vscode from 'vscode';
-import * as Parser from "web-tree-sitter"
+import * as path from "path";
+import * as vscode from "vscode";
+import * as Parser from "web-tree-sitter";
 
 function getTopLevelForm(root: Parser.SyntaxNode, offset: number): vscode.Selection | null {
-    const f = root.children.find(
-        (n) => (offset <= n.endIndex) && (offset >= n.startIndex))
+  const f = root.children.find((n) => offset <= n.endIndex && offset >= n.startIndex);
 
-    if (!f) {
-        return null
-    }
+  if (!f) {
+    return null;
+  }
 
-    return new vscode.Selection(
-        f.startPosition.row,
-        f.startPosition.column,
-        f.endPosition.row,
-        f.endPosition.column
-    )
+  return new vscode.Selection(
+    f.startPosition.row,
+    f.startPosition.column,
+    f.endPosition.row,
+    f.endPosition.column
+  );
+}
+
+function getPrecedingForm(root: Parser.SyntaxNode, offset: number): vscode.Selection | null {
+  // selects the preceding function call or name
+  const f = root.children.find((n) => offset <= n.endIndex && offset >= n.startIndex);
+
+  if (!f) {
+    return null;
+  }
+
+  return new vscode.Selection(
+    f.startPosition.row,
+    f.startPosition.column,
+    f.endPosition.row,
+    f.endPosition.column
+  );
 }
 
 async function initParser(): Promise<Parser> {
-    await Parser.init()
+  await Parser.init();
 
-    const langFile = path.join(__dirname, '..', "assets", "tree-sitter-python.wasm")
-    const parser = new Parser();
-    const Lang = await Parser.Language.load(langFile)
-    parser.setLanguage(Lang)
-    return parser
+  const langFile = path.join(__dirname, "..", "assets", "tree-sitter-python.wasm");
+  const parser = new Parser();
+  const Lang = await Parser.Language.load(langFile);
+  parser.setLanguage(Lang);
+  return parser;
 }
 
 export { getTopLevelForm, initParser };
---
+++ src/test/unit_tests/parser.test.ts
@@ -0,0 +1,8 @@
+import * as assert from "assert";
+
+suite("Unit Test - Parser", () => {
+  test("Sample test", () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+});
