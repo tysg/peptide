a8a9ea73c5f3ae9f810a44164e3262898a6aa9c4
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-12-27 15:55:34 +0800

    Format with prettier

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -1,46 +1,41 @@
-import * as vscode from 'vscode';
-import * as Parser from "web-tree-sitter"
-import { getTopLevelForm } from "./parser"
+import * as vscode from "vscode";
+import * as Parser from "web-tree-sitter";
+import { getTopLevelForm } from "./parser";
 
 // Commands related to evaluation
 
 async function evalTopLevelForm(parser: Parser) {
-    let editor = vscode.window.activeTextEditor
-    if (!editor) {
-        return 
-    }
-    let doc = editor.document
+  let editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  let doc = editor.document;
 
+  const tree = parser.parse(doc.getText()).rootNode;
 
-    const tree = parser.parse(doc.getText()).rootNode
+  // find form at cursor
+  const p = editor.selection.active;
 
-    // find form at cursor
-    const p = editor.selection.active
+  const form = getTopLevelForm(tree, doc.offsetAt(p));
+  if (!form) {
+    console.error("No parent found for current cursor");
+    return;
+  }
 
-    const form = getTopLevelForm(tree, doc.offsetAt(p))
-    if (!form) {
-        console.error("No parent found for current cursor")
-        return
-    }
-
-    editor.selection = form
-    vscode.commands.executeCommand("python.execSelectionInTerminal")
+  editor.selection = form;
+  vscode.commands.executeCommand("python.execSelectionInTerminal");
 }
-
 
 async function evalCurrentFile(parser: Parser) {
-    let editor = vscode.window.activeTextEditor
-    if (!editor) {
-        return 
-    }
-    let doc = editor.document
-    const tree = parser.parse(doc.getText()).rootNode
+  let editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  let doc = editor.document;
+  const tree = parser.parse(doc.getText()).rootNode;
 
-    editor.selection = new vscode.Selection(doc.positionAt(0), doc.positionAt(tree.endIndex)) 
-    vscode.commands.executeCommand("python.execSelectionInTerminal")
+  editor.selection = new vscode.Selection(doc.positionAt(0), doc.positionAt(tree.endIndex));
+  vscode.commands.executeCommand("python.execSelectionInTerminal");
 }
-
-
-
 
 export { evalTopLevelForm, evalCurrentFile };
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,24 @@
-import * as vscode from 'vscode';
-import { evalTopLevelForm, evalCurrentFile } from './evaluate';
-import { initParser } from './parser'
+import * as vscode from "vscode";
+import { evalTopLevelForm, evalCurrentFile } from "./evaluate";
+import { initParser } from "./parser";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	const parser = await initParser()
+  const parser = await initParser();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "peptide" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "peptide" is now active!');
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('peptide.evalTopLevelForm', () => evalTopLevelForm(parser)));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("peptide.evalTopLevelForm", () => evalTopLevelForm(parser))
+  );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('peptide.evalCurrentFile', () => evalCurrentFile(parser)));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("peptide.evalCurrentFile", () => evalCurrentFile(parser))
+  );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
